d0921669839512c07659f040bb3e5fa0a563ff25
Sébastien Le Callonnec <sebastien@weblogism.com>
2012-11-26 06:35:56 +0000

    First attempt at handling nested function calls.

--- runtime.c
+++ runtime.c
@@ -21,7 +21,6 @@ signed int mul(signed int a, signed int b)
 
 signed int ne(signed int a, signed int b)
 {
-    //  printf("na: %d != %d %d", a, b, (a!=b));
   return a != b;
 }
 
@@ -39,7 +38,6 @@ signed int not(signed int a)
 // evaluation happens before this function is called.
 signed int and(signed int a, signed int b)
 {
- printf("and: %d && %d %d", a, b, (a&&b));
   return a && b;
 }
 
